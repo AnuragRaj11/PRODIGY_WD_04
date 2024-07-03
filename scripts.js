d4d43c98de095cfb97b5da56db5c0b83c038b4f0
AnuragRaj11 <Araj59519@gmail.com>
2024-07-04 01:51:41 +0530

    My Portfolio

---
+++ scripts.js
@@ -0,0 +1,17 @@
+window.addEventListener('scroll', function () {
+    const nav = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
+
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll('nav ul li a');
+const menuLength = menuItem.length;
+for (let i = 0; i < menuLength; i++) {
+    if (menuItem[i].href === currentLocation) {
+        menuItem[i].className = 'active';
+    }
+}
